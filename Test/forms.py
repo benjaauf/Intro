c39e54b0bfb77dacda510d2cfc5596d4cbc0a6b7
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-11-21 15:08:58 -0300

    Update forms.py

--- Test/forms.py
+++ Test/forms.py
@@ -1,5 +1,14 @@
+'''
 from django import forms
 
 class Ingresar_ramo(forms.Form):
     ramo = forms.CharField(max_length=40)
+'''
+from django import forms
+from .models import Ramo
 
+class Ingresar_ramo(forms.ModelForm):
+
+    class Meta:
+        model = Ramo
+        fields = ['name']
