52b4d8ad61847f75933c60d98d5ba1c017afb731
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-11-21 15:09:02 -0300

    Update models.py

--- Test/models.py
+++ Test/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Ramo(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
