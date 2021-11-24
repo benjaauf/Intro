3ec43f10162e74c3e43fa679022164d391d92d3a
Benjaauf <benjaauf@gmail.com>
2021-11-23 23:08:26 -0300

    horario terminado

--- Test/forms.py
+++ Test/forms.py
@@ -1,45 +1,49 @@
 from django import forms
 from .models import Ramo, Horario
-from django.forms import fields
-from .models import Horario
 
 class Ingresar_ramo(forms.ModelForm):
     class Meta:
         model = Ramo
         fields = ['ramo']
 
-
-
 class LunesForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Lunes'
 
 class MartesForm(forms.ModelForm):
     class Meta:
-        model = Ramo
-        fields = ['ramo']
-        
-class HorarioForm(forms.ModelForm):
         model = Horario
         exclude = ['day']
+    prefix = 'Martes'
+
 class MiercolesForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Miercoles'
+
 class JuevesForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Jueves'
+
 class ViernesForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Viernes'
+
 class SabadoForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Sabado'
+
 class DomingoForm(forms.ModelForm):
     class Meta:
         model = Horario
         exclude = ['day']
+    prefix = 'Domingo'
--- Test/views.py
+++ Test/views.py
@@ -27,13 +27,13 @@ def test(request):
 
 def horario(request):
     if request.method == 'POST':
-        lunes = LunesForm(request.POST, instance= Horario(day = 'Lunes'))
-        martes = MartesForm(request.POST,instance= Horario(day = 'Martes'))
-        miercoles = MiercolesForm(request.POST,instance= Horario(day = 'Miercoles'))
-        jueves = JuevesForm(request.POST,instance= Horario(day = 'Jueves'))
-        viernes = ViernesForm(request.POST,instance= Horario(day = 'Viernes'))
-        sabado = SabadoForm(request.POST,instance= Horario(day = 'Sabado'))
-        domingo = DomingoForm(request.POST,instance= Horario(day = 'Domingo'))
+        lunes = LunesForm(request.POST, instance= Horario(day = 'Lunes'),auto_id='l%s')
+        martes = MartesForm(request.POST,instance= Horario(day = 'Martes'),auto_id='ma%s')
+        miercoles = MiercolesForm(request.POST,instance= Horario(day = 'Miercoles'), auto_id='mi%s')
+        jueves = JuevesForm(request.POST,instance= Horario(day = 'Jueves'), auto_id='j%s')
+        viernes = ViernesForm(request.POST,instance= Horario(day = 'Viernes'),auto_id='v%s')
+        sabado = SabadoForm(request.POST,instance= Horario(day = 'Sabado'), auto_id='s%s')
+        domingo = DomingoForm(request.POST,instance= Horario(day = 'Domingo'), auto_id='d%s')
         if lunes.is_valid() and martes.is_valid() and miercoles.is_valid() and jueves.is_valid() and viernes.is_valid() and sabado.is_valid() and domingo.is_valid():
             lunes.save()
             martes.save() 
@@ -44,13 +44,13 @@ def horario(request):
             domingo.save() 
         return redirect('horario')
     else:
-        lunes = LunesForm()
-        martes = MartesForm()
-        miercoles = MiercolesForm()
-        jueves = JuevesForm()
-        viernes = ViernesForm()
-        sabado = SabadoForm()
-        domingo = DomingoForm()
+        lunes = LunesForm(auto_id='l%s')
+        martes = MartesForm(auto_id='ma%s')
+        miercoles = MiercolesForm( auto_id='mi%s')
+        jueves = JuevesForm(auto_id='j%s')
+        viernes = ViernesForm(auto_id='v%s')
+        sabado = SabadoForm(auto_id='s%s')
+        domingo = DomingoForm(auto_id='d%s')
     context= {'Lunes':lunes,'Martes':martes,'Miercoles':miercoles,'Jueves':jueves,'Viernes':viernes,'Sabado': sabado,'Domingo':domingo}
     return render(request, 'Test/hora.html',context)
 
