1183239032d843eef3d2d79b6b9ac2fc8ad7085f
BaalKa <94659633+BaalKa@users.noreply.github.com>
2021-11-24 03:02:43 -0300

    Update models.py

--- Test/models.py
+++ Test/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
+class Ramo(models.Model):
+    ramo = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.ramo
+
+DISP=[
+    ('ocupado', 'Ocuapado'),
+    ('libre','Libre'),
+]
 # Create your models here.
+
+class Horario(models.Model):
+    day = models.CharField(max_length=20)
+    b1 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b2 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b3 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b4 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b5 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b6 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b7 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b8 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b9 = models.CharField(max_length=20, choices=DISP, default='libre')
+    b10 = models.CharField(max_length=20, choices=DISP, default='libre')
+
+<<<<<<< Updated upstream
+    def __str__(self):
+        return self.day
+=======
+class Descanso(models.Model):
+    hora = models.DurationField()
+>>>>>>> Stashed changes
