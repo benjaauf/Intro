de5c2d44ce9d1543509a77bc888fb5cbd061b2d5
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-11-23 21:52:59 -0300

    Update urls.py

--- Test/urls.py
+++ Test/urls.py
@@ -3,7 +3,6 @@ from Test import views
 
 urlpatterns = [
     path('', views.test, name='test'),
-    path('lista/',views.lista,name='lista'),
     path('delete/<int:ramo_id>/',views.delete,name='delete'),
     path('horario/', views.horario, name='horario'),
 ]
