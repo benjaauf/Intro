8faa2280363c9c80ac3a387c5c4a7c6b937c9e07
Benjaauf <benjaauf@gmail.com>
2021-11-28 13:13:10 -0300

    puse un menu de inicio

--- perfiles/urls.py
+++ perfiles/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from perfiles import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('home/',views.home, name='h'),
+    path('home/',views.home, name='home'),
+    path('register/',views.register, name='register'),
+    path('',views.inicio, name='inicio'),
+    path('login/', LoginView.as_view(template_name='perfiles/login.html'),name='login'),
+    path('logout/', LogoutView.as_view(template_name='perfiles/inicio.html'), name='logout'),
+
     # path
     ]
--- perfiles/views.py
+++ perfiles/views.py
@@ -1,7 +1,27 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import  messages
+from .forms import UserRegisterForm
 # Create your views here.
 
 def home(request):
-   return render(request, 'home.html')
+   return render(request, 'perfiles/home.html')
 
+def logout(request):
+   return redirect('inicio')
+
+def register(request):
+   if request.method == 'POST':
+      form = UserRegisterForm(request.POST)
+      if form.is_valid():
+         form.save()
+         # username = form.cleaned_data['username']
+         # messages.success(request, f'Usuario {username} creado')
+         return redirect('inicio')
+   else:
+      form = UserRegisterForm()
+   context = {'form':form}
+   return render(request, 'perfiles/register.html', context)
+
+def inicio(request):
+   return render(request, 'perfiles/inicio.html')
