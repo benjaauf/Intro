e347bf2fd471332a37dc45408f67ac7e212df0e7
Benjaauf <benjaauf@gmail.com>
2021-11-30 08:51:17 -0300

    Conecte parte del Test2

--- Test/urls.py
+++ Test/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.test, name='test'),
     path('delete/<int:ramo_id>/',views.delete,name='delete'),
     path('descanso/',views.descanso,name='descanso'),
-    path('updatet1/', views.updatet1, name='updatet1')
+    path('updatet1/', views.updatet1, name='updatet1'),
+    path('deletet1/<int:ramo_id>/',views.deletet1,name='deletet1')
 ]
--- Test2/views.py
+++ Test2/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from Test.forms import Ingresar_ramo
 from .forms import Test2
+from Test.models import *
 
 
 def test2(request):
-	f = Test2()
-	return render(request,'test2.html',{'form': f})
+	user = request.user
+	ramos = Ramo.objects.filter(user=user) 
+	materias = []
+	temp=[]
+	context = {'user':user}
+	for ramo in ramos:
+		materias.append(ramo)
+	for materia in materias:
+		temp.append(materia.ramo)
+	materias = temp
+	temp=[]
+	forms = []
+	if request.method == 'POST':
+		for materia in materias:
+			ins = Ramo.objects.get(user= user, ramo= materia)
+			form = Test2(request.POST,instance=ins,prefix=materia)
+			if form.is_valid():
+				form.save()
+				forms.append(form)
+		return redirect('home')	
+	else:
+		for materia in materias:
+			ins = Ramo.objects.get(user= user, ramo= materia)
+			form= Test2(instance=ins,prefix=materia)
+			forms.append(form)
+	context= {'user':user, 'materias':materias,'ramos':ramos,'forms':forms}
+	return render(request,'Test2/test2.html', context)
