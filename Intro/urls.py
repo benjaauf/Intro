baba96d5835222a17cb430ba0a9dc27649847730
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-11-30 09:30:44 -0300

    Update urls.py

--- Intro/urls.py
+++ Intro/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('perfiles.urls')),
     path('test/', include('Test.urls')),
     path('test2/', include('Test2.urls')),
+    path('avatar/',include('Avatar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
