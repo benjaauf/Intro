f9b328b49b2c4f11152803bd3e9aee337e4aae04
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-01 20:17:57 -0300

    Update models.py

--- Avatar/models.py
+++ Avatar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class accesorios(models.Model):
+class Accesorios(models.Model):
     name = models.CharField(max_length=15)
     imagen = models.ImageField(upload_to="accesorios")
 
@@ -11,7 +11,7 @@ class accesorios(models.Model):
         return self.name
 
 
-class caras(models.Model):
+class Caras(models.Model):
     name = models.CharField(max_length=15)
     avatar = models.ImageField(upload_to="caras")
 
@@ -19,16 +19,9 @@ class caras(models.Model):
         return self.name
 
 
-class vestuario(models.Model):
+class Vestuario(models.Model):
     name = models.CharField(max_length=15)
     avatar = models.ImageField(upload_to="vestuario")
-
-    def __str__(self):
-        return self.name
-
-class polis(models.Model):
-    name = models.CharField(max_length=15)
-    avatar = models.ImageField(upload_to="polis")
 
     def __str__(self):
         return self.name
