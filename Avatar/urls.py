0f202b9ab018f892e11f48b6fb2a7682fa885647
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-01 21:49:38 -0300

    avatar no funcional

--- Avatar/urls.py
+++ Avatar/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from Avatar import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.avatar,name='avatar'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
