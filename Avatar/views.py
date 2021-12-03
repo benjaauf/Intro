786a1668ac8df019413154bc96c77cba07904f2f
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-03 12:19:57 -0300

    ehee

--- Avatar/views.py
+++ Avatar/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import *
 
-conacc = [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,34]
+conacc = [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
 concar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 conver = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
@@ -9,36 +9,54 @@ def deracc(request):
     accesorio = contador.objects.get(name='acceso')
     accesorio.valor = accesorio.valor +1
     accesorio.save()
+    if accesorio.valor >= 17:
+        accesorio.valor = 0
+        accesorio.save()
     return redirect('avatar')
     
 def dercar(request):
     cara = contador.objects.get(name='careta')
     cara.valor = cara.valor +1
     cara.save()
+    if cara.valor >= 14:
+        cara.valor = 0
+        cara.save()
     return redirect('avatar')
 
 def derves(request):
     ropa = contador.objects.get(name='vesto')
     ropa.valor = ropa.valor +1
     ropa.save()
+    if ropa.valor >= 14:
+        ropa.valor = 0
+        ropa.save()
     return redirect('avatar')
 
 def izqacc(request):
     accesorio = contador.objects.get(name='acceso')
     accesorio.valor = accesorio.valor -1
     accesorio.save()
+    if accesorio.valor <= -1:
+        accesorio.valor = 16
+        accesorio.save()
     return redirect('avatar')
 
 def izqcar(request):
     cara = contador.objects.get(name='careta')
     cara.valor = cara.valor -1
     cara.save()
+    if cara.valor <= -1:
+        cara.valor = 13
+        cara.save()
     return redirect('avatar')
 
 def izqves(request):
     ropa = contador.objects.get(name='vesto')
     ropa.valor = ropa.valor -1
     ropa.save()
+    if ropa.valor <= -1:
+        ropa.valor = 13
+        ropa.save()
     return redirect('avatar')
 
 def avatar(request):
