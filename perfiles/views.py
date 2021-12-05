030a7481826b8b629c05b0e9fcc1601343584e21
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-05 00:44:31 -0300

    avatar en home

--- perfiles/views.py
+++ perfiles/views.py
@@ -5,15 +5,25 @@ from .forms import UserRegisterForm
 from Test.models import *
 from .calendario import crear_calendario
 
+from Avatar.models import Accesorios,Caras,Vestuario,contador
 
+conacc = [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
+concar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+conver = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 # Create your views here.
 
 def home(request):
+   ac = conacc[contador.objects.get(name='acceso').valor]
+   ca = concar[contador.objects.get(name='careta').valor]
+   ve = conver[contador.objects.get(name='vesto').valor]
+   accesorios = Accesorios.objects.get(id=ac)
+   caras = Caras.objects.get(id=ca)
+   vestuario = Vestuario.objects.get(id=ve)
    user = request.user
    ramos = Ramo.objects.all().filter(user=user).order_by('id')
    horario = Horario.objects.all().filter(user=user)
-   context = {'user':user,'ramos':ramos,'horario':horario}
+   context = {'user':user,'ramos':ramos,'horario':horario,'accesorios':accesorios, 'caras':caras, 'vestuario':vestuario}
    return render(request, 'perfiles/home.html',context)
 
 def logout(request):
