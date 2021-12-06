d2e8a4043b15a684d78c30d2d13cdaf5888d914f
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-06 15:29:08 -0300

    niveles sin suma

--- perfiles/models.py
+++ perfiles/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Nivel(models.Model):
+    name= models.CharField(max_length=10)
+    numero = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Exp(models.Model):
+    name= models.CharField(max_length=10)
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- perfiles/views.py
+++ perfiles/views.py
@@ -1,3 +1,4 @@
+from os import name
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import  authenticate,login
@@ -5,7 +6,7 @@ from .forms import UserRegisterForm
 from Test.models import *
 from .calendario import crear_calendario, crear_estudio
 from django.contrib import messages
-
+from .models import *
 from Avatar.models import Accesorios,Caras,Vestuario,contador
 
 conacc = [35,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
@@ -15,6 +16,16 @@ conver = [18, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17]
 # Create your views here.
 
 def home(request):
+   nivel=Nivel.objects.get(name='nivelon')
+   exp = Exp.objects.get(name='experience')
+   supera = Exp.objects.get(name='supera')
+   if exp.valor >= supera.valor:
+      nivel.numero = nivel.numero + 1
+      nivel.save()
+      supera.valor = supera.valor + 15
+      supera.save()
+      exp.valor = 0
+      exp.save()
    ac = conacc[contador.objects.get(name='acceso').valor]
    ca = concar[contador.objects.get(name='careta').valor]
    ve = conver[contador.objects.get(name='vesto').valor]
@@ -24,7 +35,7 @@ def home(request):
    user = request.user
    ramos = Ramo.objects.all().filter(user=user).order_by('id')
    horario = Horario.objects.all().filter(user=user)
-   context = {'user':user,'ramos':ramos,'horario':horario,'accesorios':accesorios, 'caras':caras, 'vestuario':vestuario}
+   context = {'user':user,'ramos':ramos,'horario':horario,'accesorios':accesorios, 'caras':caras, 'vestuario':vestuario,'nivel':nivel,'exp':exp,'supera':supera}
    return render(request, 'perfiles/home.html',context)
 
 def logout(request):
@@ -40,6 +51,36 @@ def register(request):
          user = authenticate(request,username=username, password =password)
          login(request,user)
          calendario = crear_calendario()
+
+
+         ##############################################################
+
+         accesorio = contador.objects.get(name='acceso')
+         accesorio.valor = 0
+         accesorio.save()
+
+         cara = contador.objects.get(name='careta')
+         cara.valor = 0
+         cara.save()
+
+         ropa = contador.objects.get(name='vesto')
+         ropa.valor = 0
+         ropa.save()
+
+         nivel = Nivel.objects.get(name='nivelon')
+         nivel.numero = 1
+         nivel.save()
+
+         exp = Exp.objects.get(name='experience')
+         exp.valor = 0
+         exp.save()
+
+         supera = Exp.objects.get(name='supera')
+         supera.valor = 15
+         supera.save()
+
+         ##############################################################
+
          return redirect('test')
    else:
       form = UserRegisterForm()
