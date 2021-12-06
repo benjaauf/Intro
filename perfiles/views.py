e5805b6524cc3a6d6d7ac0346b729f5fbd0b3650
Caarlost <carlos.bracamonte@sansano.usm.cl>
2021-12-06 18:59:06 -0300

    goku drip

--- perfiles/views.py
+++ perfiles/views.py
@@ -9,9 +9,9 @@ from django.contrib import messages
 from .models import *
 from Avatar.models import Accesorios,Caras,Vestuario,contador
 
-conacc = [35,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
-concar = [18, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17]
-conver = [18, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17]
+conacc = [35,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,36]
+concar = [18, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,19]
+conver = [18, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,19]
 
 # Create your views here.
 
